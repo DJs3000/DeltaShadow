b6121abf07c8946717d3e5e7cf722f173c87004e
djs3000 <djs500@mail.ru>
2022-06-20 05:04:59 +0300

    X11 remove

--- Environment/PrimaryScreen.cpp
+++ Environment/PrimaryScreen.cpp
@@ -1,27 +1,38 @@
 #include "PrimaryScreen.h"
-#include <X11/Xlib.h>
 #include <stdint.h>
 
 namespace Environment {
 namespace PrimaryScreen {
 
+//bool Size(int32_t &width, int32_t &height)
+//{
+//   bool res = false;
+//
+//   Display *d = XOpenDisplay(NULL);
+//   if (d)
+//   {
+//      Screen  *s = XDefaultScreenOfDisplay(d);
+//      width  = s->width;
+//      height = s->height;
+//      res = true;
+//   }
+//   XCloseDisplay(d);
+//
+//   return res;
+//}
+
 bool Size(int32_t &width, int32_t &height)
 {
    bool res = false;
 
-   Display *d = XOpenDisplay(NULL);
-   if (d)
-   {
-      Screen  *s = XDefaultScreenOfDisplay(d);
-      width  = s->width;
-      height = s->height;
-      res = true;
-   }
-   XCloseDisplay(d);
+//SDL_Surface *Screen;
+//atexit(SDL_Quit);
+//if( SDL_Init(SDL_INIT_VIDEO) < 0 ) exit(1);
+//SDL_WM_SetCaption("SDL Window", NULL);
+//Screen = SDL_SetVideoMode( 640 , 480 , 32 , SDL_DOUBLEBUF|SDL_HWSURFACE|SDL_ANYFORMAT);
 
    return res;
 }
-
 
 } // namespace Screen
 } // namespace Environment
